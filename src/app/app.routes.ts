3e8fb6e3ef50fff5cc951638eb986e9932304e77
FelippeSobral <lippesobral@hotmail.com>
2025-03-18 10:49:33 -0300

    fix:

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { BrandListComponent } from './brands/brands-list/brands-list.component';
+import { VehiclesListComponent } from './vehicles/vehicles-list/vehicles-list.component';
 
 export const routes: Routes = [
+  { path: 'vehicles', component: VehiclesListComponent},
   { path: 'brands', component: BrandListComponent }, // Rota para a lista de marcas
   { path: '', redirectTo: '/brands', pathMatch: 'full' }, // Rota padrão
   { path: '**', redirectTo: '/brands' } // Rota de fallback
