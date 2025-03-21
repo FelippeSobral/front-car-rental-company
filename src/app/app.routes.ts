a789255a03f777f15d282acba794b7ac2b227d5a
FelippeSobral <lippesobral@hotmail.com>
2025-03-21 10:02:53 -0300

    raw: mudanças nas configs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,18 @@ import { BrandListComponent } from './brands/brands-list/brands-list.component';
 import { VehiclesListComponent } from './vehicles/vehicles-list/vehicles-list.component';
 import { CategoriesListComponent } from './category/categories-list/categories-list.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
+
 
 export const routes: Routes = [
-  { path: 'dashboard', component: DashboardComponent},
-  { path: 'categories', component: CategoriesListComponent},
-  { path: 'vehicles', component: VehiclesListComponent},
-  { path: 'brands', component: BrandListComponent }, // Rota para a lista de marcas
-  { path: '', redirectTo: '/brands', pathMatch: 'full' }, // Rota padrão
-  { path: '**', redirectTo: '/brands' } // Rota de fallback
+
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'categories', component: CategoriesListComponent },
+  { path: 'vehicles', component: VehiclesListComponent },
+  { path: 'brands', component: BrandListComponent },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: '**', redirectTo: '/dashboard' }
 ];
--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,13 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideToastr } from 'ngx-toastr';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideRouter(routes) // Configura as rotas
+    provideRouter(routes), // Configura as rotas
+    provideAnimations(),   // Necessário para ngx-toastr
+    provideToastr(),       // Configuração do Toastr
   ]
 }).catch(err => console.error(err));
